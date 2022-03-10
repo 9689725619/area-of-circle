e33898a9881863d2bfe20e6d76eec7871b973440
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-10 18:05:51 +0530

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  	int area,radius;
+	
+printf("ener radius of circle");
+scanf("%d",&radius);
+
+area=3.1452*radius*radius;
+
+printf("area of circle:%d",area);
+return(0);
+	
+}
+
